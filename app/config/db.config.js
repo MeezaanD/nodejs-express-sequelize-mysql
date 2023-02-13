a3f2df1892122bca0ea4b3f52eabef6657c93e26
Meezaan Davids <meezaandavids365@gamil.com>
2023-02-13 12:16:35 +0200

    first

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "bvdn0w9qxip8t7zqtsf5-mysql.services.clever-cloud.com",
+    USER: "uxmapdpftn7rgnqz",
+    PASSWORD: "0pQxTlAONDB34AKa8DUz",
+    DB: "bvdn0w9qxip8t7zqtsf5",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
